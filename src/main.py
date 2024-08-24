e38c7bc5b19a2aa07644ee4dcd82b8a625e5b8a8
miguel <miguel.gil2005@gmail.com>
2024-08-23 21:11:23 -0300

    fasfsa

--- src/main.py
+++ src/main.py
@@ -1,3 +1,22 @@
+def operar_carro():
+    print('1- Ligar motor')
+    print('2- Desligar motor')
+    print('3- Acelerar')
+
+    op = 0
+    while op not in (1, 2, 3):
+        op = int(input("Digite as opcoes[1-3]: "))
+
+        if op == 1:
+            carro1.ligar()
+        elif op == 2:
+            carro1.desligar()
+        elif op == 3:
+            v = float(input("Informe a velocidade: "))
+        t = float(input("Informe o tempo: "))
+        carro1.acelerar(v, t)
+
+
 from frota import *
 
 if __name__ == "__main__":
@@ -5,45 +24,43 @@ if __name__ == "__main__":
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
+
     kms = float(input('Digite com quantos Kms: '))
-    litros = float(input("Informe o nivel do tanque: "))
-    cm = float(input("COnsumo medio: "))
 
-    print('Cadastre outro carro')
-    nm_modelo2 = input('Digite o modelo: ')
-    nm_marca2 = input('Digite a marca: ')
-    nm_cor2 = input('Digite a cor: ')
-    kms2= float(input('Digite com quantos Kms: '))
-    litros2 = float(input("Informe o nivel do tanque: "))
-    cm2 = float(input("COnsumo medio: "))
+    carro1 = Carro(nm_modelo, nm_marca, nm_cor, kms, motor=False)
 
-    carro1 = Carro(nm_modelo, nm_marca, nm_cor, kms, True, litros, cm)
-    carro2 = Carro(nm_modelo2, nm_marca2, nm_cor2, kms2, True, litros2, cm2)
+    print('Cadastre um carro')
+    nm_modelo = input('Digite o modelo: ')
+    nm_marca = input('Digite a marca: ')
+    nm_cor = input('Digite a cor: ')
 
+    kms = float(input('Digite com quantos Kms: '))
 
+    carro2 = Carro(nm_modelo, nm_marca, nm_cor, kms, motor=False)
 
     '''
-    Controlando o carro até ele atingir 10000km ou 600km
-
+    Controlando o carro até ele atingir 600 Km
     '''
-    while carro1.odometro < 600 and carro2.odometro < 600 and (carro1.tanque > 0  or carro2.tanque > 0):
-        escolha = input('Escolha: carro1 ou carro2')
+    while carro1.odometro < 600 and carro2.odometro < 600:
         try:
-          op = carro1
 
+            op = 0
+            while op not in (1, 2):
+                op = int(input("Qual carro [1,2]? "))
 
-    
+            if op == 1:
+                operar_carro(carro1)
+            else:
+                operar_carro(carro2)
 
-              
-
-
+            print('Infos atuais do carro')
+            print(carro1)
         except Exception as e:
-                print("Erro!")
-                print(e)
+            print("Erro!")
+            print(e)
 
-
-
-
-
-
-
+    carro1.desligar()
+    carro2.desligar()
+    print(carro1)
+    print(carro2)
+    print('Parar para trocar óleo!!!')
