05099abffa189f82fec1c4d01af4678c5ddc12e5
miguel <miguel.gil2005@gmail.com>
2024-08-23 21:04:01 -0300

    fasfsa

--- src/main.py
+++ src/main.py
@@ -6,15 +6,19 @@ if __name__ == "__main__":
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
     kms = float(input('Digite com quantos Kms: '))
+    litros = float(input("Informe o nivel do tanque: "))
+    cm = float(input("COnsumo medio: "))
 
     print('Cadastre outro carro')
     nm_modelo2 = input('Digite o modelo: ')
     nm_marca2 = input('Digite a marca: ')
     nm_cor2 = input('Digite a cor: ')
     kms2= float(input('Digite com quantos Kms: '))
+    litros2 = float(input("Informe o nivel do tanque: "))
+    cm2 = float(input("COnsumo medio: "))
 
-    carro1 = Carro(nm_modelo, nm_marca, nm_cor, kms, motor = True)
-    carro2 = Carro(nm_modelo2, nm_marca2, nm_cor2, kms2, motor = True)
+    carro1 = Carro(nm_modelo, nm_marca, nm_cor, kms, True, litros, cm)
+    carro2 = Carro(nm_modelo2, nm_marca2, nm_cor2, kms2, True, litros2, cm2)
 
 
 
